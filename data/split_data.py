f5113b8f1e425cc3b7436e98ee6d6cd1104c5885
tothanhtung0205 <tothanhtung0205@gmail.com>
2018-05-17 19:25:43 +0700

    add test byhand data

--- data/split_data.py
+++ data/split_data.py
@@ -1,18 +1,20 @@
 from io import open
 
 aaa = []
-with open("qa_eg020.txt","r",encoding="utf-8") as f:
+with open("train_50.txt","r",encoding="utf-8") as f:
     for line in f:
+       line = line.split('--->')[0]
        aaa.append(line)
 
 
-with open("test_50.txt","w",encoding="utf-8") as f_w:
+with open("byhand_test_50.txt","w",encoding="utf-8") as f_w:
     for sen in aaa[:50]:
         f_w.write(sen)
+        f_w.write(u'\n')
     f_w.close()
 
 
-with open("train_50.txt","w",encoding="utf-8") as f_w:
-    for sen in aaa[50:]:
-        f_w.write(sen)
-    f_w.close()
+# with open("train_50.txt","w",encoding="utf-8") as f_w:
+#     for sen in aaa[50:]:
+#         f_w.write(sen)
+#     f_w.close()
